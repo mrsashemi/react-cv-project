f94f4bb77d101123e48445c0f9d28316214f6187
mrsashemi <mrsashemi@gmail.com>
2022-08-11 00:01:22 -0700

    Additional updates to formatting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,7 +20,7 @@ export class ContactForm extends Component {
                     <input onChange={this.props.handleChange} value={jobSeeker.address} name="address" type="text" id="addressInput" placeholder='Address or City' /><br></br>
                     <input onChange={this.props.handleChange} value={jobSeeker.phone} name="phone" type="tel" id="phoneInput" placeholder='Phone Number' /><br></br>
                     <input onChange={this.props.handleChange} value={jobSeeker.email} name="email" type="email" id="emailInput" placeholder='Email' /><br></br>
-                    <textarea onChange={this.props.handleChange} value={jobSeeker.objective} name="objective" id="objectiveInput" placeholder='Objective Statement' rows="4" maxLength="150" /><br></br>
+                    <textarea onChange={this.props.handleChange} value={jobSeeker.objective} name="objective" id="objectiveInput" placeholder='Objective Statement' rows="4" maxLength="250" /><br></br>
                     <input onChange={this.props.handleKeywords} value={keyword.text} type="text" id="keywordInput" placeholder='Keywords' /><br></br>
                     <button onClick={this.props.addKeyword} type="button" >Add Keyword</button><br></br>
                     <br></br><button type="submit">{contactText}</button>
@@ -43,8 +43,8 @@ export class ContactInfo extends Component {
             <div className='personalInfo'>
                 <h1>{jobSeeker.firstName} {jobSeeker.lastName}</h1>
                 <h4>{jobSeeker.address}</h4>
-                <h4>{jobSeeker.phone}</h4>
-                <h4>{jobSeeker.email}</h4>
+                <h5>{jobSeeker.phone}</h5>
+                <h5>{jobSeeker.email}</h5>
                 <div className="objective">{jobSeeker.objective}</div>
                 <ul>
                     {keywords.map((keyword) => {
--- src/components/Education.js
+++ src/components/Education.js
@@ -12,9 +12,9 @@ export class EducationForm extends Component {
             <div>
                 <form onSubmit={this.props.onSubmitEdu}>
                     <br></br><label htmlFor="educationInput">Education: </label><br></br>
-                    <input onChange={this.props.handleEdu} value={education.college} name="college" type="text" id="collegeInput" placeholder='University' /><br></br>
+                    <input onChange={this.props.handleEdu} value={education.college} name="college" type="text" id="collegeInput" placeholder='University or Institution' /><br></br>
                     <input onChange={this.props.handleEdu} value={education.collegeCity} name="collegeCity" type="text" id="collegeCityInput" placeholder='City' /><br></br>
-                    <input onChange={this.props.handleEdu} value={education.degree} name="degree" type="text" id="degreeInput" placeholder='Degree' /><br></br>
+                    <input onChange={this.props.handleEdu} value={education.degree} name="degree" type="text" id="degreeInput" placeholder='Degree or Certificate' /><br></br>
                     <input onChange={this.props.handleEdu} value={education.major} name="major" type="text" id="majorInput" placeholder='Major' /><br></br>
                     <input onChange={this.props.handleEdu} value={education.minor} name="minor" type="text" id="minorInput" placeholder='Minor' /><br></br>
                     <input onChange={this.props.handleEdu} value={education.collegeStart} name="collegeStart" type="text" id="collegeStartInput" placeholder='Start Date' /><br></br>
@@ -40,10 +40,9 @@ export class EducationInfo extends Component {
         return (
             <div className='educationPoint'>
                 <div className="hidden" ref={this.props.hiddenEdu}>
-                    <h3>{education.degree} in {education.major}</h3>
-                    {education.minor !== '' && <h4>with minors in {education.minor}</h4>}
-                    <h4>{education.college} in {education.collegeCity}</h4>
-                    <h5>{education.collegeStart} to {education.collegeEnd}</h5>
+                    <h4 className="inlineText">{education.college} / {education.degree} in {education.major} </h4> 
+                    {education.minor !== '' && <h5 className="inlineText"> with minors in {education.minor}</h5>}
+                    <h6>{education.collegeStart} to {education.collegeEnd}, {education.collegeCity}</h6>
                     <ul>
                         {education.notes.map((note) => {
                             return <li key={note.id}>{note.text}</li>
@@ -53,10 +52,9 @@ export class EducationInfo extends Component {
                 {educations.map((education) => {
                     return (
                         <div key={education.id}>
-                            <h3>{education.degree} in {education.major} </h3> 
-                            {education.minor !== '' && <h4>with minors in {education.minor}</h4>}
-                            <h4>{education.college} in {education.collegeCity}</h4>
-                            <h5>{education.collegeStart} to {education.collegeEnd}</h5>
+                            <h4 className="inlineText">{education.college} / {education.degree} in {education.major} </h4> 
+                            {education.minor !== '' && <h5 className="inlineText"> with minors in {education.minor}</h5>}
+                            <h6>{education.collegeStart} to {education.collegeEnd}, {education.collegeCity}</h6>
                             <ul>
                                 {education.notes.map((note) => {
                                     return <li key={note.id}>{note.text}</li>
